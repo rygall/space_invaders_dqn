b5fc4a74fce42f6a2ec383c0f72bb4c0db424a99
Ryan Adreani <ryanadreani@gmail.com>
2023-09-05 12:41:57 -0500

    Updated the actions saving matrix to only be 1 x maxEpochs rather than maxEpisodes x maxEpochs since we're saving it every episode now

--- training.py
+++ training.py
@@ -10,18 +10,19 @@ agent = dqn.DQN()
 env = gym.make("ALE/SpaceInvaders-v5", obs_type="grayscale")
 
 # define training parameters
-max_epochs = 34
+max_epochs = 100000
 max_episodes = 100
 observation, info = env.reset()
 
-episode_actions = np.zeros((max_episodes, max_epochs))
-curr_actions = np.zeros(max_epochs)
+actions = np.zeros(max_epochs)
 
 for episode in range(max_episodes):
     print("Episode", episode)
     observation, info = env.reset()
 
     for epoch in range(max_epochs):
+        if epoch % 10 == 0:
+            print("Epoch", epoch)
 
         # get through first 33 frames since nothing happens
         if epoch < 33:
@@ -30,7 +31,7 @@ for episode in range(max_episodes):
         
         # get next action from DQN
         action = agent.action(observation)
-        curr_actions[epoch] = action
+        actions[epoch] = action
 
         # take a step in the environment
         observation, reward, terminated, truncated, info = env.step(action)
@@ -46,11 +47,11 @@ for episode in range(max_episodes):
             observation, info = env.reset()
             break
 
-    episode_actions[episode] = curr_actions
-    curr_actions = np.zeros(max_epochs)
-
-        # save agent weights and actions
-    np.save("episodeActions/Episode-Actions_" + str(episode) + ".npy", episode_actions)
+    # save agent weights and actions
+    np.save("episodeActions/actions_" + str(episode) + ".npy", actions)
+    
+    actions = np.zeros(max_epochs)
+    
     agent.save(episode)
 
 # close the environment  
